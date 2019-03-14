6b9d015a49efdd3cd5912140a91a1975a386c24b
Nate Tan <nktan93@gmail.com>
2019-03-14 17:49:42 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -22,7 +22,7 @@ pigeon_data = {
 def nyc_pigeon_organizer(data)
   new_hash = {}
   data.each do |category, category_hash|
-    category_hash.each do |type, members
+    category_hash.each do |type, members|
     binding.pry
       new_hash[members][category] = []
     end
